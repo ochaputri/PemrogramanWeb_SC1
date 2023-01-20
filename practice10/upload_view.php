f852684ff36866e111acebdd81087a268d378f82
ochaputri <ochaptr02@gmail.com>
2023-01-20 15:00:56 +0700

    first commit

---
+++ practice10/upload_view.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+</head>
+<body>
+    <form action="upload_process.php" method="post" enctype="multipart/form-data">
+       Pilih Gambar:
+       <input type="file" name="gambar_contoh" id="gambar_contoh">
+       <input type="submit" name="submit">
+    </form>
+</body>
+</html>
